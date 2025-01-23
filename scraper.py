3f664416e7252f0f81a8e6ed3768f8f92be54250
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-01-22 22:56:58 -0300

    Scraper finished

--- scraper.py
+++ scraper.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import re
 import os
-from datetime import datetime, timedelta
-import time
+from datetime import date
+
 
 # Configuração do diretório de trabalho
 os.chdir(r"C:\Users\joaov\Documents\NLP Model")
+
 
 # Função para obter o HTML de uma página
 def get_html(url):
@@ -15,12 +15,12 @@ def get_html(url):
     response.raise_for_status()
     return BeautifulSoup(response.text, 'lxml')
 
-def scraper():
 
-    # DataFrame para armazenar os imóveis exibidos
-    displayed_properties = pd.DataFrame()
+def cnn_scraper():
 
-    ### Pedro Granado Imóveis
+    # CNN Brasil
+
+    df = pd.DataFrame(columns=['date','journal','title','text'])
 
     main_page = get_html("https://www.cnnbrasil.com.br/economia/")
 
@@ -41,13 +41,68 @@ def scraper():
             full_text = ''
             for text_block in text_array:
                 full_text = full_text + text_block.text + ' '
-            print(full_text)
+            full_text = full_text.strip()
+
+            df_page = pd.DataFrame([{
+                'date':date.today().strftime("%d-%m-%Y"),
+                'journal':'CNN Brasil',
+                'title':title,
+                'text':full_text
+            }])
+
+            df = pd.concat([df, df_page], ignore_index=True)
+
+    return df
 
 
+def infomoney_scraper():
+
+    # InfoMoney
+    
+    df = pd.DataFrame(columns=['date','journal','title','text'])
+
+    main_page = get_html("https://www.infomoney.com.br/economia/")
+
+    pages = main_page.select("div.w-full[data-ds-component='card-sm']")
+
+    for page in pages:
+
+        link = page.select('a')[1]['href']
+        title = page.select('a')[1].text.strip()
+
+        news_page = get_html(link)
+
+        text_array = news_page.select('article')[0].select('p:not([class])')
+
+        full_text = ''
+        for text_block in text_array:
+            full_text = full_text + text_block.text + ' '
+        full_text = full_text.strip()
+
+        df_page = pd.DataFrame([{
+            'date':date.today().strftime("%d-%m-%Y"),
+            'journal':'InfoMoney Brasil',
+            'title':title,
+            'text':full_text
+        }])
+
+        df = pd.concat([df, df_page], ignore_index=True)
+
+    return df
 
 
+def get_news():
+    cnn_news = cnn_scraper()
+    infomoney_news = infomoney_scraper()
+
+    news = pd.concat([cnn_news, infomoney_news], ignore_index=True)
+
+    return news
 
 
+past_news = pd.read_csv("Brazilian News Database.csv", sep=';')
 
+today_news = get_news()
 
-scraper()
+news = pd.concat([past_news, today_news], ignore_index=True)
+news.to_csv("Brazilian News Database.csv", sep=';', index=False)
