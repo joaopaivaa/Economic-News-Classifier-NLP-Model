f48e8436e851a34f577cc247b14f39b8b9bfb359
João Paiva <joaovmarcotti@hotmail.com>
2025-04-26 17:14:23 +0000

    Update Brazilian News Database.csv

--- auto_push.sh
+++ auto_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git add .
-git commit -m "Update repository"
+git commit -m "Update Brazilian News Database.csv"
 git push origin main
--- run_auto_push.py
+++ run_auto_push.py
@@ -1,3 +1,7 @@
 import subprocess
+from datetime import datetime
 
-subprocess.run(["bash", "/home/joaopaiva/Economic-News-Classifier-NLP-Model/auto_push.sh"])
+if True:
+    subprocess.run(["bash", "/home/joaopaiva/Economic-News-Classifier-NLP-Model/auto_push.sh"])
+else:
+    print("Today is not Friday. No push was made.")
