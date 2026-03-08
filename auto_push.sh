434739247d56ef0fb28c512193993fdf35a1e549
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2026-03-08 02:14:20 -0300

    remove unused files

--- auto_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git pull
-
-git add .
-git commit -m "Update Brazilian News Database.csv"
-git push origin main
--- run_auto_push.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-from datetime import datetime
-
-if datetime.now().weekday() == 4:
-    subprocess.run(["bash", "/home/joaopaiva/Economic-News-Classifier-NLP-Model/auto_push.sh"])
-else:
-    print("Today is not Friday. No push was made.")
